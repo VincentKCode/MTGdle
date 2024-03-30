6b32f9beb63e5e681e487da3e1fdcef5bff1f203
VSoups <vince.kubota@gmail.com>
2024-03-29 23:01:04 -0700

    Final confirmation test

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ let board;
 let guessCount;
 let winner;
 let hidCard; // Hidden card
+// let trueAttr; // attributes of hidden card to compare (probably unnecessary)
 let guessCard;
+// let prevGuessCards; // cards already chosed (probably unecessary)
 let valMatch; // Matching values of card (might be redundant)
 
 /*----- cached elements  -----*/
