d1b0c4e31474fc0ab45d204823fe54ab6c958c9a
VSoups <vince.kubota@gmail.com>
2024-04-01 21:13:34 -0700

    Guess comparison functions working

--- js/main.js
+++ js/main.js
@@ -3,86 +3,86 @@ const cardList = [
     {
         cardArtMini: 'images/black-lotus-CUT.jpg',
         cardArtFull: 'images/black-lotus.png',
-        cardName: 'Black Lotus',
+        cardName: 'black lotus',
         color: ['Colorless'],
         cmc: 0,
         type: ['Artifact'],
         legendary: false,
         origin: 'Alpha\n1993',
-        keywords: ['Mana Generator', 'Ramp', 'Self-sacrifice'],
+        keywords: ['Mana Generator', 'Ramp', 'Self-sacrifice', 'Tap Ability'],
         hint: 'Former record holder for most expensive card...',
     },
     {
-        cardArtMini: '',
-        cardArtFull: '',
-        cardName: 'Sol Ring',
+        cardArtMini: 'images/sol-ring-CUT.jpg',
+        cardArtFull: 'images/sol-ring.png',
+        cardName: 'sol ring',
         color: ['Colorless'],
         cmc: 1,
         type: ['Artifact'],
         legendary: false,
         origin: 'Alpha\n1993',
-        keywords: ['Mana Generator', 'Ramp'],
+        keywords: ['Mana Generator', 'Ramp', 'Tap Ability'],
         hint: 'Every commander deck has one...'
     },
     {
         cardArtMini: '',
         cardArtFull: '',
-        cardName: 'Rhystic Study',
+        cardName: 'rhystic study',
         color: ['Blue'],
         cmc: 3,
         type: ['Enchantment'],
         legendary: false,
         origin: 'Prophecy\n2000',
-        keywords: ['Card Draw'],
+        keywords: ['Card Draw', 'Mana Tax'],
         hint: '"Will you pay the one?"'
     },
     {
         cardArtMini: '',
         cardArtFull: '',
-        cardName: 'The One Ring',
+        cardName: 'the one ring',
         color: ['Colorless'],
         cmc: 4,
         type: ['Artifact'],
         legendary: true,
         origin: 'Tales of Middle Earth\n2023',
-        keywords: ['Card Draw, Self-burn, Protection, Indestructable'],
-        hint: '"Post Malone\'s most expensive card..."'
+        keywords: ['Card Draw', 'Self-burn', 'Protection', 'Indestructable', 'Tap Ability'],
+        hint: 'Post Malone\'s most expensive card...'
     },
     {
         cardArtMini: '',
         cardArtFull: '',
-        cardName: 'Test Idx 4', // placeholder
-        color: ['Blue'],
-        cmc: 3,
-        type: ['Enchantment'],
+        cardName: 'swords to plowshares',
+        color: ['White'],
+        cmc: 1,
+        type: ['Instant'],
         legendary: false,
-        origin: 'Prophecy\n2000',
-        keywords: ['Card Draw'],
-        hint: '"Will you pay the one?"'
+        origin: 'Alpha\n1993',
+        keywords: ['Spot Removal', 'Exile', 'Life Gain'],
+        hint: 'White commander staple...'
     },
     {
         cardArtMini: '',
         cardArtFull: '',
-        cardName: 'Test Idx 5', // placeholder
-        color: ['Blue'],
-        cmc: 3,
-        type: ['Enchantment'],
-        legendary: false,
-        origin: 'Prophecy\n2000',
-        keywords: ['Card Draw'],
-        hint: '"Will you pay the one?"'
+        cardName: 'griselbrand',
+        color: ['Black'],
+        cmc: 8,
+        type: ['Creature'],
+        legendary: true,
+        origin: 'Avacyn Restored\n2012',
+        keywords: ['Flying', 'Lifelink', 'Card Draw', 'Pay Life', 'Activated Ability'],
+        hint: 'Makes Sheoldred look weak in comparison...'
     },
     {
         cardArtMini: '',
         cardArtFull: '',
-        cardName: 'Test Idx 6', // placeholder
-        color: ['Blue'],
-        cmc: 3,
-        type: ['Enchantment'],
+        cardName: 'lightning bolt',
+        color: ['Red'],
+        cmc: 1,
+        type: ['Instant'],
         legendary: false,
-        origin: 'Prophecy\n2000',
-        keywords: ['Card Draw'],
-        hint: '"Will you pay the one?"'
+        origin: 'Alpha\n1993',
+        keywords: ['Burn', 'Any Target'],
+        hint: '"____ the bird..."'
     },
 ];
 
@@ -103,6 +103,7 @@ let textInputEl = document.getElementById('search-bar');
 const gameTimerEl = document.getElementById('timer');
 const guessCountEl = document.querySelectorAll('.guess-count');
 const replayBtnEl = document.querySelector('button');
+const cardGridEl = document.getElementById('card-grid');
 
 /*----- event listeners -----*/
 textInputEl.addEventListener('keypress', handleGuess);
@@ -114,15 +115,18 @@ function init() {
     board = []; // will contain img for small art and obj for each attribute being compared
     guessCount = 0;
     winner = null; // 1-win, null-game in progress
-    hidCard = rndCardPicker(); // currently holding card name in cardList array
+    hidCard = rndCardPicker(); // currently holding card object from cardList array
     guessCard = {}; // obj for saving cards that have been guessed so player cant select them again
     render();
 }
 
 function rndCardPicker() { // WORKING
-    let rndCardIdx = Math.floor(Math.random() * cardList.length);
-    let rndCard = cardList[rndCardIdx].cardName;
-    return rndCard;
+    // let rndCardIdx = Math.floor(Math.random() * cardList.length);
+    // let rndCard = cardList[rndCardIdx];
+    // // alert(rndCard);
+    // return rndCard;
+
+    return cardList[0]; // set to Black Lotus for attr match testing
 }
 
 /*----- render functions -----*/
@@ -131,12 +135,12 @@ function render() {
     getWinner(); // might make renderHitCard useless
     renderHidCard(); // stay hidden until winner = 1
     renderGuessCount(); // take guessCard.length + 1(correct 0 idx count)
-    // renderCardSheet(); // card list cheat sheet for easy mode (might be iceboxxed)
+    // renderCardSheet(); // card list cheat sheet for easy mode (might be iceboxed)
     renderDropMenu(); // card dropdown menu when player types
 }
 
 function renderBoard() {
-
+    newRow(cardGridEl); // create new row in grid
 }
 
 function getWinner() {
@@ -160,6 +164,54 @@ function renderDropMenu() {
 function handleGuess(evt) {
     if (evt.key !== 'Enter') return;
     evt.preventDefault();
-    alert(textInputEl.value.toUpperCase()); // placeholder function test
+    let cardGuess = textInputEl.value.toLowerCase();
+    // alert(typeof(cardGuess)); // placeholder function test
+    nameFilter(cardGuess);
+    return cardGuess;
+}
+
+function nameFilter(cardGuess) { // match cardList card obj to input name
+    for (const card of cardList) {
+        // need to make current card name a variable for equality to work properly for some reason
+        let listCard = card.cardName;
+        if (hidCard.cardName === cardGuess) { // check for winner
+            console.log('WINNER');
+            break;
+        } else if (listCard === cardGuess) { // name input to card in list array
+            // console.log(listCard);
+            compareCards(card);
+        } 
+    }
+}
+
+function compareCards(cardObj) { // access attributes of wrong card guess for comparison
+    console.log('path true');
+    for (let attr in cardObj) {
+        let secAttrKey = hidCard[attr];
+        // console.log(secAttrKey)
+        if (cardObj[attr] === secAttrKey) { // check for non array values
+            console.log(`${attr} | ${cardObj[attr]} = true`);
+        } else if (typeof(cardObj[attr]) === "object") { // check array for matches
+            compareArrs(cardObj[attr], secAttrKey, attr)
+        } else console.log(`${attr} | ${cardObj[attr]} = false`); // no match
+
+        // if (typeof(cardObj[attr]) === "object") {
+        //     compareArrs(cardObj[attr], secAttrKey, attr)
+        // }
+    }
+}
+                  //  array | hidcard arr | key
+function compareArrs(attrArray, secArray, refAttr) {    
+    let i = 0;
+    while(i < secArray.length) { // WORKING // checks attributes containing arrays for individual matches
+        if (attrArray.includes(secArray[i])) {
+            console.log(`${refAttr} includes ${secArray[i]}`);
+            // console.log(attrArray);
+        } 
+        i++;
+    }
+}
+
+function newRow(cardGrid) {
     
 }
